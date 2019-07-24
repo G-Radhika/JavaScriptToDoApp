050d364186f0f422062e0e5791a2ed0fff389538
G-Radhika <radhik.g@gmail.com>
2019-07-24 11:44:04 -0500

    Update fireStoreDB.js

--- js/fireStoreDB.js
+++ js/fireStoreDB.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBNJXr5VxDC1JguCmxUTjLf0mY1TxxDM78",
-    authDomain: "todo-5c511.firebaseapp.com",
-    databaseURL: "https://todo-5c511.firebaseio.com",
-    projectId: "todo-5c511",
-    storageBucket: "todo-5c511.appspot.com",
-    messagingSenderId: "5975036936",
-    appId: "1:5975036936:web:fe51e346d7a4c3af"
+    apiKey: "<your Key>",
+    authDomain: "<>",
+    databaseURL: "<>",
+    projectId: "<>",
+    storageBucket: "<>",
+    messagingSenderId: "<id>",
+    appId: "<id>"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
